b4282204fd6e1ef9843d35deabfa1b8a6d5c415c
Garima Tiwari <garimatiwari1803@gmail.com>
2025-10-31 19:31:08 +0530

    fix: comment out unused FooterPage import and component in LandingPage

--- my-project/src/components/landingpage.jsx
+++ my-project/src/components/landingpage.jsx
@@ -1,7 +1,6 @@
 import Explore from "./explore";
 import FAQ from "./Faq";
-import FooterPage from "./footerpage";
-import FooterPage from "./footerpage";
+
 
 import Hero from "./hero";
 import List from "./list";
@@ -19,7 +18,7 @@ export default function LandingPage() {
         <Yt/>
         <Explore/> 
         <FAQ/> 
-        <FooterPage/>
+        {/* <FooterPage/> */}
         
         </>
     )
